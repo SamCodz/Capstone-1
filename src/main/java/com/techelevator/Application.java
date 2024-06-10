47e42a0ad16c55e14a653c5692ab8763b7ed188d
Jamille <jamillebowen@yahoo.com>
2024-06-10 00:54:41 -0400

    preparing

--- src/main/java/com/techelevator/Application.java
+++ src/main/java/com/techelevator/Application.java
@@ -1,8 +1,25 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class Application {
+	public static Scanner input = new Scanner(System.in);
+	public static int userInput;
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+
+		menu();
+	}
+
+	public static int menu(){
+		System.out.println("****************************************************************************");
+		System.out.println("OUR VENDING MACHINE");
+		System.out.println("****************************************************************************\n");
+		System.out.println("(1) Display Vending Machine Items");
+		System.out.println("(2) Purchase");
+		System.out.println("(3) Exit");
+		userInput = input.nextInt();
+
+
 	}
 }
